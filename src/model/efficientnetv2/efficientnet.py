5c405ad4987252873a17e5bedd1fc5c1220f14f3
Askar Tsyganov <ashtsyganov@edu.hse.ru>
2025-01-01 17:09:10 +0300

    Fixes

--- src/model/efficientnetv2/efficientnet.py
+++ src/model/efficientnetv2/efficientnet.py
@@ -1,9 +1,11 @@
+from torch import nn
 from torchvision.models import efficientnet_v2_l, efficientnet_v2_m, efficientnet_v2_s
 
 
-class EfficientNetV2:
+class EfficientNetV2(nn.Module):
     def __init__(self, type, *args, **kwargs):
         assert type in ["small", "medium", "large"]
+        super(EfficientNetV2, self).__init__()
 
         if type == "small":
             self.model = efficientnet_v2_s(*args, **kwargs)

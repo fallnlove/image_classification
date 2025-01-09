e025d529032a24f4d5c1d563ef4820d4b8ebb91e
Askar Tsyganov <ashtsyganov@edu.hse.ru>
2025-01-09 14:53:36 +0300

    Add wideresnet with convs

--- src/model/wideresnet/wideresnet.py
+++ src/model/wideresnet/wideresnet.py
@@ -1,20 +1,12 @@
 from torch import Tensor, nn
 from torchvision.models import wide_resnet50_2
-from torchvision.models.resnet import Bottleneck, _resnet
 
 
 class WideResNet(nn.Module):
-    def __init__(self, width_per_group: int = 128, kernel_size: int = 3, **kwargs):
+    def __init__(self, kernel_size: int = 3, **kwargs):
         super(WideResNet, self).__init__()
 
-        self.model = _resnet(
-            Bottleneck,
-            [2, 2, 2, 2],
-            None,
-            True,
-            width_per_group=width_per_group,
-            **kwargs
-        )
+        self.model = wide_resnet50_2(**kwargs)
 
         self.model.conv1 = nn.Conv2d(
             3, 64, kernel_size=kernel_size, padding="same", bias=False
